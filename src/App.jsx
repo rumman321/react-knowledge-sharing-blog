40faefde57beb14eb3cc93df8bae45b571661f93
rumman321 <cset-210652@dti.ac>
2024-10-22 13:36:43 +0600

    make fake data and fetch all data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
 
 function App() {
   
@@ -7,7 +9,8 @@ function App() {
   return (
     <>
      
-      <h1 className='text-2xl font-bold'>Vite + React</h1>
+      <Header></Header>
+      <Blogs></Blogs>
       
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-2xl font-bold'>Vite + React</h1>
+        </div>
+    );
+};
+
+export default Header;
